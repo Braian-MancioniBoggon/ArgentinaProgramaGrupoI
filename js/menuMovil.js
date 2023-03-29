8c0a5fd3d153417a1a1d604442ed4cf0a7308008
Cesar <cesarprincipi73@gmail.com>
2023-03-28 22:48:47 -0300

    agregado de slider en js

--- js/menuMovil.js
+++ js/menuMovil.js
@@ -1,6 +1,29 @@
+window.addEventListener('load', function(){
+const  slider=document.getElementById("slider");
+const imagenes =["imange1.jpg", "imagen2.jpg","imagen3.jpg"];
+let index = 0;
+
+const cambiarImagenes= function(){
+    var camino='imagenes/' + imagenes[index];
+    slider.src= camino;
+
+    if (index < imagenes.length-1)
+    {
+        index ++;
+    }
+    else{
+        index = 0;
+    }
+}
+cambiarImagenes();
+this.setInterval(cambiarImagenes,5000);0
+
+
+
 const toggleButton = document.getElementsByClassName(navbar-toggle)[0];
 const navbarLinks = document.getElementsByClassName(navbar-links);
 toggleButton.addEventListener('click', function () {
     for (var i = 0; i < navbarLinks.length; i++)
         navbarLinks[i].classList.toggle('active');
 });
+})
