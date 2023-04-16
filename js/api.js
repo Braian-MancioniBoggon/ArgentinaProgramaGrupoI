022d424a9b54e12b5090aafabf8b9df2db6dd585
Braian-MancioniBoggon <braian.mancioni@municipiodetandil.edu.ar>
2023-04-16 13:07:08 -0300

    16/4 cambios en wizard y estilos varios

---
+++ js/api.js
@@ -0,0 +1,29 @@
+//Dolar Argentino
+const dolarBlue = document.querySelector("#dolarBlue");
+const dolarObicial = document.querySelector("#dolarObicial");
+
+dolarBlue.addEventListener("click", function(){obtenerDatos("Blue");});
+dolarObicial.addEventListener("click", function(){obtenerDatos("Oficial");});
+
+let obtenerDatos = (dolar) => {
+  let url = `https://www.dolarsi.com/api/api.php?type=dolar`;
+  const api = new XMLHttpRequest();
+  api.open("GET", url, true);
+  api.send();
+
+  api.onreadystatechange = function(){
+    if(this.status == 200 && this.readyState == 4){
+      let datos = JSON.parse(this.responseText);
+      let mostrarDolar = document.querySelector("#mostrarDolar");
+      mostrarDolar.innerHTML = "";
+
+      for (let tipoDolar of datos){
+        if (tipoDolar.casa.nombre === dolar){
+          mostrarDolar.innerHTML += ` <h4 id="cotizacionTitulo">Dolar ${dolar}</h4>
+                                      <p id="cotizacionCompra">Compra: <span>$${tipoDolar.casa.compra.slice(0, 6)}</span></p>
+                                      <p id="cotizacionVenta">Venta: <span>$${tipoDolar.casa.venta.slice(0, 6)}</span></p>`
+        }
+      }
+    }
+  }
+};
--- js/wizard.js
+++ js/wizard.js
@@ -1,28 +1,27 @@
 //Declaro todos los elementos que voy a utilizar
-let nombrePaso1 = document.getElementById("nombrePaso1");
-let mailPaso1 = document.getElementById("mailPaso1");
-let modeloPaso2 = document.getElementById("modeloPaso2");
-let problemaPaso2 = document.getElementById("problemaPaso2");
-let nombrePaso3 = document.getElementById("nombrePaso3");
-let mailPaso3 = document.getElementById("mailPaso3");
-let modeloPaso3 = document.getElementById("modeloPaso3");
-let problemaPaso3 = document.getElementById("problemaPaso3");
-let paso1 = document.getElementById("paso1");
-let paso2 = document.getElementById("paso2");
-let paso3 = document.getElementById("paso3");
-let btnPaso1 = document.getElementById("btnPaso1");
-let btnPaso2 = document.getElementById("btnPaso2");
-let btnPaso3 = document.getElementById("btnPaso3");
-let avisoDeEnvio = document.getElementById("avisoDeEnvio");
-let btnVolverPaso1 = document.getElementById("btnVolverPaso1");
-let btnModificar = document.getElementById("btnModificar");
-
-//Vuerlvo al paso 1 para modificar los datos ingresados
-let volverPaso1 = () => {
-    paso1.style.display="flex";
-    paso2.style.display="none";
-    paso3.style.display="none";
-}
+const nombrePaso1 = document.getElementById("nombrePaso1");
+const mailPaso1 = document.getElementById("mailPaso1");
+const modeloPaso2 = document.getElementById("modeloPaso2");
+const problemaPaso2 = document.getElementById("problemaPaso2");
+const nombrePaso3 = document.getElementById("nombrePaso3");
+const mailPaso3 = document.getElementById("mailPaso3");
+const modeloPaso3 = document.getElementById("modeloPaso3");
+const problemaPaso3 = document.getElementById("problemaPaso3");
+const paso1 = document.getElementById("paso1");
+const paso2 = document.getElementById("paso2");
+const paso3 = document.getElementById("paso3");
+const btnPaso1 = document.getElementById("btnPaso1");
+const btnPaso2 = document.getElementById("btnPaso2");
+const btnPaso3 = document.getElementById("btnPaso3");
+const avisoDeEnvio = document.getElementById("avisoDeEnvio");
+const btnVolverPaso1 = document.getElementById("btnVolverPaso1");
+const btnModificar = document.getElementById("btnModificar");
+const guiaPaso1 = document.getElementById("guiaPaso1");
+const iconoPaso1 = document.getElementById("iconoPaso1");
+const guiaPaso2 = document.getElementById("guiaPaso2");
+const iconoPaso2 = document.getElementById("iconoPaso2");
+const guiaPaso3 = document.getElementById("guiaPaso3");
+const iconoPaso3 = document.getElementById("iconoPaso3");
 
 //Guardo los valores de los inputs del paso 1 en los inputs del paso 3 ademas de ocultar el paso 1 y mostrar el paso 2
 let verPaso2 = () => {
@@ -31,6 +30,9 @@ let verPaso2 = () => {
         mailPaso3.value=mailPaso1.value;
         paso1.style.display="none";
         paso2.style.display="flex";
+        guiaPaso1.classList.toggle("pasoActual");
+        iconoPaso1.innerHTML=`<i class="las la-check-circle"></i>`;
+        guiaPaso2.classList.toggle("pasoActual");
     }
 };
 
@@ -41,6 +43,9 @@ let verPaso3 = () => {
         problemaPaso3.value=problemaPaso2.value;
         paso2.style.display="none";
         paso3.style.display="flex";
+        guiaPaso2.classList.toggle("pasoActual");
+        iconoPaso2.innerHTML=`<i class="las la-check-circle"></i>`;
+        guiaPaso3.classList.toggle("pasoActual");
     }
 };
 
@@ -52,9 +57,37 @@ let verPaso1 = () => {
     mailPaso1.value="";
     modeloPaso2.value="";
     problemaPaso2.value="";
+    guiaPaso3.classList.toggle("pasoActual");
+    iconoPaso3.innerHTML=`<i class="las la-dot-circle"></i>`;
+    guiaPaso1.classList.toggle("pasoActual");
+    iconoPaso1.innerHTML=`<i class="las la-dot-circle"></i>`;
+    iconoPaso2.innerHTML=`<i class="las la-dot-circle"></i>`;
     avisoDeEnvio.style.display="block";
     setTimeout(ocultarAviso,5000)
 };
+
+//Vuerlvo al paso 1 para modificar los datos ingresados
+let volverPaso1 = () => {
+    paso1.style.display="flex";
+    paso2.style.display="none";
+    paso3.style.display="none";
+    guiaPaso1.classList.toggle("pasoActual");
+    iconoPaso1.innerHTML=`<i class="las la-dot-circle"></i>`;
+    guiaPaso2.classList.toggle("pasoActual");
+    iconoPaso2.innerHTML=`<i class="las la-dot-circle"></i>`;
+}
+
+//Vuerlvo al paso 1 para modificar los datos ingresados
+let modificarTodo = () => {
+    paso1.style.display="flex";
+    paso2.style.display="none";
+    paso3.style.display="none";
+    guiaPaso1.classList.toggle("pasoActual");
+    iconoPaso1.innerHTML=`<i class="las la-dot-circle"></i>`;
+    guiaPaso3.classList.toggle("pasoActual");
+    iconoPaso2.innerHTML=`<i class="las la-dot-circle"></i>`;
+    iconoPaso3.innerHTML=`<i class="las la-dot-circle"></i>`;
+}
 
 //Oculto el aviso
 let ocultarAviso = () => {
@@ -71,7 +104,7 @@ btnPaso2.addEventListener("click", verPaso3);
 btnPaso3.addEventListener("click", verPaso1);
 
 //Al apretar el boton modificar va a llamar a la función "volverPaso1"
-btnModificar.addEventListener("click", volverPaso1);
+btnModificar.addEventListener("click", modificarTodo);
 
 //Al apretar el boton volver va a llamar a la función "volverPaso1"
 btnVolverPaso1.addEventListener("click", volverPaso1);
